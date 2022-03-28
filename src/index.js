635125ba0d129f52acea327b248b3080d0c5a8fb
Christina McIntyre <christinamcintyre1@gmail.com>
2022-03-28 12:57:55 -0600

    test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Spa from './App'
 import "./App.css";
- import NavBar from "./navbar.js";
- import bootstrap from "bootstrap";
+// import NavBar from "./navbar.js";
+ //import bootstrap from "bootstrap";
 
 
 ReactDOM.render(
